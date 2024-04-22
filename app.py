f15bd2d9d2dff77dd4af321335c2c2272c3cf722
Suraj Kumar Singh <75930198+suraj636@users.noreply.github.com>
2024-04-22 15:31:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,11 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
 
-# Creating an instance of FastAPI app
 app = FastAPI()
 
-# Creating a Pydantic model for request body validation
-class Numbers(BaseModel):
-    num1: float
-    num2: float
-
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to the calculator API!"}
+async def root():
+    return {"message": "Welcome to the greeting API"}
 
-
-# Defining the route for addition
-@app.post("/add")
-def add(numbers: Numbers):
-    result = numbers.num1 + numbers.num2
-    return {"result": result}
-
-# Defining the route for multiplication
-@app.post("/multiply")
-def multiply(numbers: Numbers):
-    result = numbers.num1 * numbers.num2
-    return {"result": result}
-
-# Defining the route for subtraction
-@app.post("/subtract")
-def subtract(numbers: Numbers):
-    result = numbers.num1 - numbers.num2
-    return {"result": result}
+@app.get("/greet/{name}")
+async def greet(name: str):
+    return {"message": f"Hello, {name}"}
